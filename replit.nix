4d1206cb541ecbcd4c20fccbbdc83e974d4910bd
Samad <samad@example.com>
2026-05-25 14:39:45 +0300

    simplify replit.nix: remove conflicting python deps

--- replit.nix
+++ replit.nix
@@ -1,16 +1,8 @@
 { pkgs }: {
   deps = [
-    pkgs.cmake
     pkgs.gcc
-    pkgs.openssl
-    pkgs.pkg-config
     pkgs.gnumake
     pkgs.python310
     pkgs.python310Packages.pip
-    (pkgs.python310.withPackages (ps: [
-      ps.python-telegram-bot
-      ps.requests
-      ps.python-dotenv
-    ]))
   ];
 }
